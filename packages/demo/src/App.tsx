13445714e7fe280d09bee2128a34d1d510f86a9b
Steve Strates <steve@strates.io>
2024-11-09 15:37:13 -0500

    Update demo

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { greet, Calculator } from "@channeling/seance";
+import { snakeToCamelCase } from "@channeling/seance";
 
 function App(): JSX.Element {
-  const calc = new Calculator();
-
   return (
     <div>
-      <h1>{greet("Developer")}</h1>
-      <p>2 + 2 = {calc.add(2, 2)}</p>
+      <h1>{snakeToCamelCase("boo_foo_bar")}</h1>
     </div>
   );
 }
